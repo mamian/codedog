1d9c5e25668a7df6b2878ae67776b34c98aac70a
mamian <mamianskyma@aliyun.com>
2017-02-17 23:37:50 +0800

    加密的代码类型

---
+++ src/main/java/net/mamian/codedog/enums/BaseEnum.java
@@ -0,0 +1,15 @@
+package net.mamian.codedog.enums;
+
+/**
+ * 
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2017-01-03 12:01
+ * @copyright ©2017 马面 All Rights Reserved
+ */
+public interface BaseEnum {
+
+    public String getMsg();
+
+}
---
+++ src/main/java/net/mamian/codedog/enums/FileType.java
@@ -0,0 +1,26 @@
+package net.mamian.codedog.enums;
+
+/**
+ * 
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2017-01-13 11:45
+ * @copyright ©2017 马面 All Rights Reserved
+ */
+public enum FileType implements BaseEnum {
+
+    JAVA("java代码"),
+    UNKNOWN("未知类型"),
+    ;
+
+    final String msg;
+
+    public String getMsg(){
+        return this.msg;
+    }
+
+    private FileType(String msg){
+        this.msg = msg;
+    }
+}
