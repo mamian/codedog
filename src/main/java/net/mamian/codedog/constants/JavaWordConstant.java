e751fd6e41b006aec442209c58c3c86c3df21f8e
mamian <mamianskyma@aliyun.com>
2017-02-26 23:36:22 +0800

    JavaWordConstant

---
+++ src/main/java/net/mamian/codedog/constants/JavaWordConstant.java
@@ -0,0 +1,71 @@
+package net.mamian.codedog.constants;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author zhaobs
+ * @mail zhaobeishu@meituan.com
+ * @date 2017-02-08 17:09
+ * @copyright ©2017 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+public class JavaWordConstant {
+
+    public static final Set<String> JavaWordSet = new HashSet<>();
+
+    static {
+        JavaWordSet.add("private");
+        JavaWordSet.add("protected");
+        JavaWordSet.add("public");
+        JavaWordSet.add("enum");
+        JavaWordSet.add("abstract");
+        JavaWordSet.add("class");
+        JavaWordSet.add("extends");
+        JavaWordSet.add("final");
+        JavaWordSet.add("implements");
+        JavaWordSet.add("interface");
+        JavaWordSet.add("native");
+        JavaWordSet.add("new");
+        JavaWordSet.add("static");
+        JavaWordSet.add("strictfp");
+        JavaWordSet.add("synchronized");
+        JavaWordSet.add("transient");
+        JavaWordSet.add("volatile");
+        JavaWordSet.add("break");
+        JavaWordSet.add("case");
+        JavaWordSet.add("continue");
+        JavaWordSet.add("default");
+        JavaWordSet.add("do");
+        JavaWordSet.add("else");
+        JavaWordSet.add("for");
+        JavaWordSet.add("if");
+        JavaWordSet.add("instanceof");
+        JavaWordSet.add("return");
+        JavaWordSet.add("switch");
+        JavaWordSet.add("while");
+        JavaWordSet.add("assert");
+        JavaWordSet.add("catch");
+        JavaWordSet.add("finally");
+        JavaWordSet.add("throw");
+        JavaWordSet.add("throws");
+        JavaWordSet.add("try");
+        JavaWordSet.add("import");
+        JavaWordSet.add("package");
+        JavaWordSet.add("boolean");
+        JavaWordSet.add("byte");
+        JavaWordSet.add("char");
+        JavaWordSet.add("double");
+        JavaWordSet.add("float");
+        JavaWordSet.add("int");
+        JavaWordSet.add("long");
+        JavaWordSet.add("short");
+        JavaWordSet.add("null");
+        JavaWordSet.add("super");
+        JavaWordSet.add("this");
+        JavaWordSet.add("void");
+        JavaWordSet.add("goto");
+        JavaWordSet.add("const");
+    }
+
+}
