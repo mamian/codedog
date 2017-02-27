14a96489b70f902732ed5cdd0436ee7080faace1
mamian <mamianskyma@aliyun.com>
2017-02-27 23:58:30 +0800

    符号

---
+++ src/main/java/net/mamian/codedog/constants/Separator.java
@@ -0,0 +1,148 @@
+package net.mamian.codedog.constants;
+
+/**
+ * 分隔符
+ *
+ * Created by zhaobs on 17/1/23.
+ */
+public interface Separator {
+
+    /**
+     * 点
+     * */
+    String dian = ".";
+
+    /**
+     * 逗号
+     * */
+    String douhao = ",";
+
+    /**
+     * 等号
+     * */
+    String denghao = "=";
+
+    /**
+     * 冒号
+     * */
+    String maohao = ":";
+
+    /**
+     * 问号
+     * */
+    String wenhao = "?";
+
+    /**
+     * 分号
+     * */
+    String fenhao = ";";
+
+    /**
+     * 引号
+     * */
+    String yinhao = "\"";
+
+    /**
+     * 叹号
+     * */
+    String tanhao = "!";
+
+    /**
+     * 且
+     * */
+    String yu = "&&";
+
+    /**
+     * 或
+     * */
+    String huo = "||";
+
+    /**
+     * 位运算：与
+     * */
+    String bit_yu = "&";
+
+    /**
+     * 位运算：或
+     * */
+    String bit_huo = "|";
+
+    /**
+     * 位运算：取反
+     * */
+    String bit_qufan = "^";
+
+    /**
+     * 右移位
+     * */
+    String yiwei_you = ">>";
+
+    /**
+     * 左移位
+     * */
+    String yiwei_zuo = "<<";
+
+    /**
+     * 右移位
+     * */
+    String yiwei_big_you = ">>>";
+
+    /**
+     * 左移位
+     * */
+    String yiwei_big_zuo = "<<<";
+
+    /**
+     * 转义符
+     * */
+    String zhuanyi = "\\";
+
+    /**
+     * 注解
+     * */
+    String zhujie = "@";
+
+    /**
+     * 前大括号
+     * */
+    String kuohao_big_prefix = "{";
+
+    /**
+     * 后大括号
+     * */
+    String kuohao_big_suffix = "}";
+
+    /**
+     * 前中括号
+     * */
+    String kuohao_middle_prefix = "[";
+
+    /**
+     * 后中括号
+     * */
+    String kuohao_middle_suffix = "]";
+
+    /**
+     * 前小括号
+     * */
+    String kuohao_small_prefix = "(";
+
+    /**
+     * 后小括号
+     * */
+    String kuohao_small_suffix = ")";
+
+    /**
+     * 前尖括号
+     * */
+    String kuohao_angle_prefix = "<";
+
+    /**
+     * 后尖括号
+     * */
+    String kuohao_angle_suffix = ">";
+
+
+
+
+}
