c55f2674b7cc9f3a4804231c5658fedd9e2ccf5c
mamian <mamianskyma@aliyun.com>
2017-03-04 21:12:07 +0800

    CharFilter

---
+++ src/main/java/net/mamian/codedog/analyse/CharFilter.java
@@ -0,0 +1,22 @@
+package net.mamian.codedog.analyse;
+
+/**
+ * 字符过滤器
+ *
+ * @author zhaobs
+ * @mail zhaobeishu@meituan.com
+ * @date 2017-02-07 16:04
+ * @copyright ©2017 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+@Deprecated
+public class CharFilter {
+
+    /**
+     * @param content 要过滤的内容
+     * */
+    public static final String filter(String content){
+        return content;
+    }
+
+}
